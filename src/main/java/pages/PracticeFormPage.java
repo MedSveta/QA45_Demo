0a7c85973e28a30b48383bd5ab4a2d61af77ce9b
MedSveta <sveta1978medved@gmail.com>
2024-12-05 23:22:50 +0200

    added PracticeFormPage, Hobbies

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -2,15 +2,19 @@ package pages;
 
 import dto.Student;
 import enums.Gender;
+import enums.Hobbies;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
-public class PracticeFormPage extends BasePage{
-    public PracticeFormPage(WebDriver driver){
+import java.util.List;
+
+public class PracticeFormPage extends BasePage {
+    public PracticeFormPage(WebDriver driver) {
 
         setDriver(driver);
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
@@ -24,14 +28,10 @@ public class PracticeFormPage extends BasePage{
     WebElement fieldEmail;
     @FindBy(xpath = "//input[@id='userNumber']")
     WebElement fieldMobile;
-    @FindBy(xpath = "//div[@id='subjectsContainer']")
+    @FindBy(xpath = "//input[@id='dateOfBirthInput']")
+    WebElement fieldDateOfBirth;
+    @FindBy(xpath = "//input[@id='subjectsInput']")
     WebElement fieldSubjects;
-    @FindBy(xpath = "//label[@for='hobbies-checkbox-1']")
-    WebElement chkHobbiesSports;
-    @FindBy(xpath = "//label[@for='hobbies-checkbox-2']")
-    WebElement chkHobbiesReading;
-    @FindBy(xpath = "//label[@for='hobbies-checkbox-3']")
-    WebElement chkHobbiesMusic;
     @FindBy(xpath = "//textarea[@id='currentAddress']")
     WebElement fieldAddress;
     @FindBy(xpath = "//div[text()='Select State']")
@@ -40,18 +40,58 @@ public class PracticeFormPage extends BasePage{
     WebElement btnCity;
 
 
-
-
-
-
-
     public void typePracticeForm(Student student) {
+        hideBanner();
+        hideFooter();
         fieldName.sendKeys(student.getName());
         fieldLastName.sendKeys(student.getLastName());
         fieldEmail.sendKeys(student.getEmail());
         typeGender(student.getGender());
         fieldMobile.sendKeys(student.getMobile());
+        typeDateOfBirth(student.getDateOfBirth());
+        typeSubjects(student.getSubjects());
+        typeHobbies(student.getHobbies());
+
         fieldAddress.sendKeys(student.getAddress());
+
+    }
+
+    private void typeHobbies(List<Hobbies> hobbies) {
+        for (Hobbies h: hobbies){
+            switch (h){
+                case MUSIC:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case SPORTS:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case READING:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+            }
+        }
+    }
+
+    private void typeSubjects(String subjects) {
+        fieldSubjects.click();
+        String []  arr= subjects.split(",");
+        for(String s: arr){
+            fieldSubjects.sendKeys(s);
+            fieldSubjects.sendKeys(Keys.ENTER);
+        }
+
+    }
+
+    private void typeDateOfBirth(String dateOfBirth) {
+        fieldDateOfBirth.click();
+        String operationSystem = System.getProperty("os.name");
+        System.out.println(operationSystem);
+        if (operationSystem.startsWith("Win"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        else if (operationSystem.startsWith("Mac"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+        fieldDateOfBirth.sendKeys(dateOfBirth);
+        fieldDateOfBirth.sendKeys(Keys.ENTER);
 
     }
 
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -2,21 +2,28 @@ package tests;
 
 import dto.Student;
 import enums.Gender;
+import enums.Hobbies;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeFormTests extends AppManager {
 
     @Test
-    public void studentRegFormPositiveTest(){
+    public void studentRegFormPositiveTest() {
+        List<Hobbies> hobbies= new ArrayList<>();
+        hobbies.add(Hobbies.SPORTS);
+        hobbies.add(Hobbies.MUSIC);
         Student student = new Student("Vasya", "Vasilev", "vasyavasilev@mail.com", Gender.OTHER,
-                "0123456789", "07 Mar 2001", "Maths",
-                "Music", "", "street 1", "NCR", "Delhi");
-      new HomePage(getDriver()).clickBtnForms();
-      new FormsPage(getDriver()).clickBtnPracticeForm();
-      new PracticeFormPage(getDriver()).typePracticeForm(student);
+                "0123456789", "07 Mar 2001", "Maths,Physics,Chemistry",
+                hobbies, "", "street 1", "NCR", "Delhi");
+        new HomePage(getDriver()).clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPracticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
     }
 }
